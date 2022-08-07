cf370c58db1cfc152b62fe098a57b4b87df24830
Sriram Anne <sriram.anne2@gmail.com>
2022-08-07 09:32:15 -0400

    base project check in

---
+++ gtmobiledevicerepair/src/main/java/com/roadforge/gtmobiledevicerepair/GtmobiledevicerepairApplication.java
@@ -0,0 +1,13 @@
+package com.roadforge.gtmobiledevicerepair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtmobiledevicerepairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtmobiledevicerepairApplication.class, args);
+	}
+
+}
---
+++ gtmobiledevicerepair/src/test/java/com/roadforge/gtmobiledevicerepair/GtmobiledevicerepairApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roadforge.gtmobiledevicerepair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtmobiledevicerepairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
